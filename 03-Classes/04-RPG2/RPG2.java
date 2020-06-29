b0ce52fc8a2fa38065b0ce75bc1f4a1cca889351
corydave <corydave@users.noreply.github.com>
2020-06-29 11:54:46 -0400

    Update RPG2.java

--- 03-Classes/04-RPG2/RPG2.java
+++ 03-Classes/04-RPG2/RPG2.java
@@ -1,9 +1,17 @@
+// VERSION 2.0.5
+
 // NAME: 
 // DATE:
 // DESC:
 
 public class RPG2 {
+  
+  public final static boolean DEBUG = false;
+  
   public static void main(String[] args) {
   
+      Character.setDebug(DEBUG);
+      Battle.setDebug(DEBUG);    
+    
   }
 }
