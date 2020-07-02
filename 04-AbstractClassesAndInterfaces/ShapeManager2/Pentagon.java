94a30339abd068637f58d6f06a9e375c0ac6c89e
corydave <corydave@users.noreply.github.com>
2020-07-02 13:57:41 -0400

    Create Pentagon.java

---
+++ 04-AbstractClassesAndInterfaces/ShapeManager2/Pentagon.java
@@ -0,0 +1,47 @@
+// STUB CODE: Dave Ghidiu
+
+public class Pentagon implements Shape {
+    
+    private int size;
+    
+    public Pentagon() {
+        size = 0;
+    }
+    
+    public Pentagon(int size) {
+        this.size = size;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+    
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    public double getArea() {
+        
+        return -1; // must change this method
+        
+    }
+    
+    public double getPerimeter() {
+        
+         return -1; // must change this method
+      
+    }
+    
+    public String toString() {
+        
+        return "This PENTAGON has a side of " + size + ", a perimeter of " + getPerimeter() + ", and an area of " + getArea();
+        
+    }
+    
+    public int compareTo(Shape s) {
+        
+        return 0; // must change this method
+      
+    }
+    
+}
