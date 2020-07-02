4249371c6defd889aafaffed79d33b8a3d536db6
corydave <corydave@users.noreply.github.com>
2020-07-02 14:52:46 -0400

    Update Shape.java
    Included version number
    Added `getSize()`
    Added `setSize()`

--- 04-AbstractClassesAndInterfaces/ShapeManager2/Shape.java
+++ 04-AbstractClassesAndInterfaces/ShapeManager2/Shape.java
@@ -1,6 +1,10 @@
 // STUB CODE: Dave Ghidiu
+// v 1.0.0
 
 public interface Shape extends Comparable<Shape> {
+    
+    public int getSize();
+    public void setSize(int size);
     
     public double getPerimeter();
     public double getArea();
