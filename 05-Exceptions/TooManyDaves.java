ee2c044d9762451f173b6a07d26e64037ca7ddce
corydave <corydave@users.noreply.github.com>
2020-07-15 18:55:55 -0400

    Create TooManyDaves.java

---
+++ 05-Exceptions/TooManyDaves.java
@@ -0,0 +1,19 @@
+// Too Many Daves by Dr. Suess
+// https://www.youtube.com/watch?v=gCsQM0KNXhU
+
+// The Daves I Know (my sister showed me this!)
+// https://www.youtube.com/watch?v=8nvzEqsZIGo
+
+
+public class TooManyDavesException extends Exception {
+
+	public TooManyDavesException() {
+		super();
+	}
+	
+	public TooManyDavesException(String message) {
+		super(message);
+	}
+	
+	
+}
